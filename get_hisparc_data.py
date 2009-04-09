27f86ad6df3e8ce349dfa4c6c1da59cc155d74e8
David Fokkema <davidf@nikhef.nl>
2009-04-09 22:32:51 +0200

    Processing of HiSPARC data is now very efficient. Table rows are created in memory and when ready, appended to the pytables table. It is approximately 1000 times faster!
    Added a lot of docstrings, :-)

--- get_hisparc_data.py
+++ get_hisparc_data.py
@@ -1,27 +1,82 @@
+""" Fetch events and eventdata from the eventwarehouse
+
+    This module fetches data from the eventwarehouse, based on supplied
+    start and end dates, limits and offsets. This data can then be passed
+    on to the process_hisparc_data module.
+
+"""
 import MySQLdb
 import datetime
 
 def get_hisparc_data(station_id=601, start=None, stop=None, limit=None,
                      offset=None):
+    """ Get events and eventdata from the eventwarehouse
+
+    This function fetches events and eventdata from the eventwarehouse
+    based on start and end dates, limits and offsets. You can pass this
+    data on to the process_events function in the process_hisparc_data
+    module.
+
+    Arguments:
+    station_id          the HiSPARC station id
+    start               a datetime instance defining the start of the
+                        search interval (inclusive)
+    stop                a datetime instance defining the end of the search
+                        interval (inclusive)
+    limit               the maximum number of events
+    offset              an offset in the total event list from which point
+                        on a limit number of events is being selected
+
+    """
     db = MySQLdb.connect('127.0.0.1', 'analysis', 'Data4analysis!',
                          'eventwarehouse', 3307)
 
+    # get events from the event table
     events = get_hisparc_events(db, station_id, start, stop, limit, offset)
 
-    if not start:
+    if not events:
+        return None, None
+
+    # in the case of an offset, determine 'start' from the event data
+    if offset:
         date = events[0][1]
         timedelta = events[0][2]
         start = datetime.datetime.combine(date, datetime.time()) + timedelta
-    if not stop:
+
+    # in the case of a limit, determine 'stop' from the event data
+    if limit:
         date = events[-1][1]
         timedelta = events[-1][2]
         stop = datetime.datetime.combine(date, datetime.time()) + timedelta
 
+    # get the eventdata, where we don't select on event_ids, but rather
+    # rely on 'start' and 'stop' instead.
     eventdata = get_hisparc_eventdata(db, station_id, start, stop)
 
     return events, eventdata
 
-def get_hisparc_events(db, station_id, start, stop, limit, offset):
+def get_hisparc_events(db, station_id, start=None, stop=None, limit=None,
+                       offset=None):
+    """ Get data from the eventd table
+
+    This function fetches data from the event table, selected by using
+    the start and stop datetime instances and possibly by using a limit and
+    offset. This routine is generally not called directly. Instead, use
+    get_hisparc_data which calls this routine and then continues to fetch
+    the corresponding eventdata.
+
+    Arguments:
+    db                  a MySQLdb connection instance to the eventwarehouse
+    station_id          the HiSPARC station id
+    start               a datetime instance defining the start of the
+                        search interval (inclusive)
+    stop                a datetime instance defining the end of the search
+                        interval (inclusive)
+    limit               the maximum number of events
+    offset              an offset in the total event list from which point on a
+                        limit number of events is being selected
+
+    """
     cursor = db.cursor()
 
     sql = "SELECT event_id, date, time, nanoseconds FROM event " \
@@ -42,20 +97,48 @@ def get_hisparc_events(db, station_id, start, stop, limit, offset):
     results = cursor.fetchall()
     return results    
 
-def get_hisparc_eventdata(db, station_id, start, stop):
+def get_hisparc_eventdata(db, station_id, start=None, stop=None):
+    """ Get data from the eventdata table
+
+    This function fetches data from the eventdata table, selected by using
+    the start and stop datetime instances. It doesn't select using a set of
+    event_id's.  This is because we may have millions of events to select
+    on. This inexact match will only be inexact at the end of the data,
+    where we possibly include eventdata from extra events. The reason is
+    that 'start' really matches the 'start' of the events, whereas 'stop'
+    has to deal with possible 'limits' and makes sure to include all
+    events, at the cost of possibly including more.
+
+    It is important to realize that YOU are responsible for making this
+    work by defining correct start and stop values.
+    
+    Of course, this is already done for you by the get_hisparc_data
+    routine, which you should probably use.
+
+    Arguments:
+    db                  a MySQLdb connection instance to the eventwarehouse
+    station_id          the HiSPARC station id
+    start               a datetime instance defining the start of the
+                        search interval (inclusive)
+    stop                a datetime instance defining the end of the search
+                        interval (inclusive)
+
+    """
     cursor = db.cursor()
 
     sql  = "SELECT event_id, uploadcode, doublevalue " \
            "FROM event e JOIN calculateddata USING(event_id) " \
            "JOIN calculateddatatype USING(calculateddatatype_id) " \
            "WHERE station_id=%d AND e.eventtype_id=1 " % station_id
-    sql += "AND date >= '%s' AND time >= '%s' " \
-           % (start.date(), start.time().strftime('%H:%M:%S'))
-    sql += "AND date <= '%s' AND time <= '%s' " \
-           % (stop.date(), stop.time().strftime('%H:%M:%S'))
     sql += "AND uploadcode " \
-           "IN ('PH1','PH2','PH3','PH4','IN1','IN2','IN3','IN4') " \
-           "ORDER BY date, time, nanoseconds"
+           "IN ('PH1','PH2','PH3','PH4','IN1','IN2','IN3','IN4') "
+    if start:
+        sql += "AND date >= '%s' AND time >= '%s' " \
+               % (start.date(), start.time().strftime('%H:%M:%S'))
+    if stop:
+        sql += "AND date <= '%s' AND time <= '%s' " \
+               % (stop.date(), stop.time().strftime('%H:%M:%S'))
+    sql += "ORDER BY date, time, nanoseconds"
     cursor.execute(sql)
     results = cursor.fetchall()
     return results    
--- process_hisparc_data.py
+++ process_hisparc_data.py
@@ -1,54 +1,84 @@
+""" Store data from the HiSPARC eventwarehouse using pytables
+
+    This module processes data read from the event and eventdata tables in
+    the eventwarehouse database. Timestamps are calculated from the date
+    and time columns (remember, in GPS time!). The eventdata columns are
+    processed and stored alongside the event data.
+
+"""
 import datetime
 import time
 import os
 
-def process_events(events, table):
+def process_events(events, eventdata, table):
+    """ Do the actual data processing and storing
+
+    This function concurrently reads the events and eventdata lists and
+    builds up the event rows. When a row is complete, i.e. there are no
+    more eventdata values for the current event row, it is stored in a
+    pytables table.
+
+    Arguments:
+    events          contents from the eventwarehouse event table
+    eventdata       contents from the eventwarehouse eventdata table
+    table           the destination table
+
+    """
     tablerow = table.row
 
-    print "Calculating and loading event timestamps..."
+    data_idx = 0
     for row in events:
+        # We process the events row by row
         event_id = row[0]
         date = row[1]
         timedelta = row[2]
         nanoseconds = row[3]
 
+        # calculate the timestamp (in GPS time, not UTC time!)
         t = datetime.datetime.combine(date, datetime.time()) + timedelta
         timestamp = time.mktime(t.utctimetuple())
 
         tablerow['event_id'] = event_id
         tablerow['timestamp'] = timestamp
         tablerow['nanoseconds'] = nanoseconds
+
+        # get default values for the eventdata
+        data = {}
+        data['pulseheights'] = tablerow['pulseheights']
+        data['integrals'] = tablerow['integrals']
+
+        while True:
+            # now process the eventdata row by row, using the current index
+            # data_idx
+            data_row = eventdata[data_idx]
+            if data_row[0] == event_id:
+                # the eventdata matches the current event, make sure to
+                # read the next eventdata row next time and process the
+                # current row
+                data_idx += 1
+                uploadcode = data_row[1]
+                value = data_row[2]
+
+                if uploadcode[:2] == 'PH':
+                    key = 'pulseheights'
+                elif uploadcode[:2] == 'IN':
+                    key = 'integrals'
+                else:
+                    continue
+                idx = int(uploadcode[2]) - 1 
+                data[key][idx] = value
+            else:
+                # The eventdata is not matching this event. Probably we've
+                # exhausted this events' eventdata. Break to store this
+                # event.
+                break
+        tablerow['pulseheights'] = data['pulseheights']
+        tablerow['integrals'] = data['integrals']
         tablerow.append()
+        # continue on to the next event
 
     table.flush()
 
-def process_eventdata(eventdata, table):
-    print "Loading event data..."
-    table_index = table.col('event_id').tolist()
-    for row in eventdata:
-        event_id = row[0]
-        uploadcode = row[1]
-        value = row[2]
-
-        if uploadcode[:2] == 'PH':
-            key = 'pulseheights'
-        elif uploadcode[:2] == 'IN':
-            key = 'integrals'
-        else:
-            continue
-
-        try:
-            tidx = table_index.index(event_id)
-        except ValueError:
-            continue
-        else:
-            data = table.read(tidx, field=key)[0]
-            idx = int(uploadcode[2]) - 1 
-            data[idx] = value
-            table.modifyColumn(tidx, column=data, colname=key)
-
-    table.flush()
-        
 if __name__ == '__main__':
     os.environ['TZ'] = 'UTC'
     time.tzset()
