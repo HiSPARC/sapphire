2e738aa9029d6fc85df19f0e2c6df05b46c37350
David Fokkema <davidf@nikhef.nl>
2011-06-23 12:19:33 +0200

    Better simulation run script
    Modified detector_sim.py to run a tiny simulation on a stand-alone system
    and ask for confirmation before overwriting previous simulation runs.

--- detector_sim.py
+++ detector_sim.py
@@ -12,6 +12,8 @@ from __future__ import division
 
 import tables
 import os.path
+import sys
+import textwrap
 
 import clusters
 from simulations import BaseSimulation, QSubSimulation
@@ -27,14 +29,27 @@ if __name__ == '__main__':
         data = tables.openFile(DATAFILE, 'a')
 
     if '/simulations' in data:
-        data.removeNode('/simulations', recursive=True)
+        print
+        print textwrap.dedent("""\
+            WARNING: previous simulations exist and will be overwritten
+            Continue? (answer 'yes'; anything else will exit)""")
+        try:
+            inp = raw_input()
+        except KeyboardInterrupt:
+            inp = 'Ctrl-C'
+
+        if inp.lower() == 'yes':
+            data.removeNode('/simulations', recursive=True)
+        else:
+            print
+            print "Aborting!"
+            sys.exit(1)
 
     sim = 'E_1PeV/zenith_0'
     cluster = clusters.SimpleCluster()
-    simulation = QSubSimulation(cluster, data,
-    #simulation = BaseSimulation(cluster, data,
+    simulation = BaseSimulation(cluster, data,
                                 os.path.join('/showers', sim, 'leptons'),
                                 os.path.join('/simulations', sim),
-                                R=100, N=100, N_cores=2)
+                                R=100, N=100)
     simulation.run()
     simulation.store_observables()
