ad214ff207300cb410423f25d63ffd81fbbfdd7e
David Fokkema <davidf@nikhef.nl>
2011-08-23 22:04:26 +0200

    Added automatic unit tests
    Added example unit tests in src/tests, along with an Eclipse builder which
    runs the unit tests each time the project changes.
    
    There's nothing actually being tested, so far.

---
+++ src/tests/test_2.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Tests(unittest.TestCase):
+    def test_first(self):
+        self.assertTrue(True)
+    
+    def test_second(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
