64076a66469400beaf64113ed42c21b2186720fb
David Fokkema <davidf@nikhef.nl>
2012-01-08 12:02:49 +0100

    Refactored ProcessEvents, new class
    Created ProcessIndexedEvents, for just processing events given in some
    index, like kascade.c_index for coincidences between KASCADE and HiSPARC
    events.

--- kascade/test.py
+++ kascade/test.py
@@ -9,6 +9,16 @@ from sapphire.analysis import process_events
 N = 1000
 
 
+def show_processing_time(events, dt, limit):
+    if limit:
+        guess_dt = len(events) / limit * dt
+    else:
+        guess_dt = dt
+        limit = len(events)
+    print "Processing %d events: %.2f s" % (limit, dt)
+    print "Estimate for total processing time: %.2f h" % (guess_dt / 3600)
+
+
 if __name__ == '__main__':
     try:
         data
@@ -16,20 +26,19 @@ if __name__ == '__main__':
         data = tables.openFile('kascade.h5', 'r')
 
     cls = process_events.ProcessEvents(
-                data, '/hisparc/cluster_kascade/station_601')
+                data, '/hisparc/cluster_kascade/station_601', limit=N)
 
     t0 = time.time()
-    ts = cls.process_traces(limit=N)
+    ts = cls.process_traces()
     dt = time.time() - t0
-    guess_dt = (len(data.root.hisparc.cluster_kascade.station_601.events) /
-                N * dt)
-    print "Processing %d events: %.2f s" % (N, dt)
-    print "Estimate for total processing time: %d s" % guess_dt
+    show_processing_time(data.root.hisparc.cluster_kascade.station_601.events,
+                         dt, N)
 
-    t0 = time.time()
+
     indexes = data.root.kascade.c_index[:,1][:N]
-    ts = cls.process_traces_from_index(indexes)
+    cls = process_events.ProcessIndexedEvents(
+                data, '/hisparc/cluster_kascade/station_601', indexes)
+    t0 = time.time()
+    ts = cls.process_traces()
     dt = time.time() - t0
-    guess_dt = len(data.root.kascade.c_index) / N * dt
-    print "Processing %d events: %.2f s" % (N, dt)
-    print "Estimate for total processing time: %d s" % guess_dt
+    show_processing_time(data.root.kascade.c_index, dt, N)
--- sapphire/analysis/process_events.py
+++ sapphire/analysis/process_events.py
@@ -9,60 +9,59 @@ ADC_THRESHOLD = 20
 ADC_TIME_PER_SAMPLE = 2.5e-9
 
 
-class ProcessEvents():
-    def __init__(self, data, group):
+class ProcessEvents(object):
+    def __init__(self, data, group, limit=None):
         self.data = data
         self.group = data.getNode(group)
+        self.limit = limit
 
-    def process_traces(self, limit=None):
+    def process_traces(self):
         """Process traces to yield pulse timing information"""
 
-        if limit:
-            events = self.group.events.iterrows(stop=limit)
+        if self.limit:
+            events = self.group.events.iterrows(stop=self.limit)
         else:
             events = self.group.events
 
-        return self.process_traces_from_event_list(events, length=limit)
+        timings = self._process_traces_from_event_list(events,
+                                                       length=self.limit)
+        return timings
 
-    def process_traces_from_index(self, indexes):
-        """Process traces to yield pulse timing information"""
-
-        events = self.group.events
-
-        return self.process_traces_from_event_list(events.itersequence(indexes),
-                                                   length=len(indexes))
-
-    def process_traces_from_event_list(self, events, length=None):
-        if length is None:
-            try:
-                length = len(events)
-            except TypeError:
-                pass
-        if length:
-            progressbar = pb.ProgressBar(maxval=length,
-                                         widgets=[pb.Percentage(), pb.Bar(),
-                                                  pb.ETA()])
-        else:
-            progressbar = lambda x: x
+    def _process_traces_from_event_list(self, events, length=None):
+        progressbar = self._create_progressbar_from_iterable(events, length)
 
         result = []
         for event in progressbar(events):
-            timings = self.reconstruct_time_from_traces(event)
+            timings = self._reconstruct_time_from_traces(event)
             result.append(timings)
         return 1e9 * np.array(result)
 
-    def reconstruct_time_from_traces(self, event):
+    def _create_progressbar_from_iterable(self, iterable, length=None):
+        if length is None:
+            try:
+                length = len(iterable)
+            except TypeError:
+                pass
+
+        if length:
+            return pb.ProgressBar(maxval=length, widgets=[pb.Percentage(),
+                                                          pb.Bar(), pb.ETA()])
+        else:
+            # Cannot create progressbar, return no-op
+            return lambda x: x
+
+    def _reconstruct_time_from_traces(self, event):
         timings = []
         for pulseheight, trace_idx in zip(event['pulseheights'],
                                           event['traces']):
             if pulseheight < ADC_THRESHOLD:
                 timings.append(np.nan)
             else:
-                trace = self.get_trace(trace_idx)
-                timings.append(self.reconstruct_time_from_trace(trace))
+                trace = self._get_trace(trace_idx)
+                timings.append(self._reconstruct_time_from_trace(trace))
         return timings
 
-    def get_trace(self, idx):
+    def _get_trace(self, idx):
         blobs = self.group.blobs
 
         trace = zlib.decompress(blobs[idx]).split(',')
@@ -71,7 +70,7 @@ class ProcessEvents():
         trace = np.array([int(x) for x in trace])
         return trace
 
-    def reconstruct_time_from_trace(self, trace):
+    def _reconstruct_time_from_trace(self, trace):
         """Reconstruct time of measurement from a trace"""
 
         t = trace[:100]
@@ -87,3 +86,16 @@ class ProcessEvents():
                 break
 
         return value * ADC_TIME_PER_SAMPLE
+
+
+class ProcessIndexedEvents(ProcessEvents):
+    def __init__(self, data, group, indexes):
+        super(ProcessIndexedEvents, self).__init__(data, group)
+        self.indexes = indexes
+
+    def process_traces(self):
+        events = self.group.events.itersequence(self.indexes)
+
+        timings = self._process_traces_from_event_list(events,
+                                                       length=len(self.indexes))
+        return timings
