786ac58ccb0d249aacae2861b87c8d8c298a5f82
David Fokkema <davidf@nikhef.nl>
2012-01-21 13:24:31 +0100

    Added constrained optimizer
    Not very succesful.  While it does keep the optimization from 'running
    away', it is not as accurate.  I used the truncated Newton optimization,
    which needs to approximate a gradient (as I don't have one).  It is less
    accurate then the Nealder-Mead simplex method, which I use for the
    unconstrained optimizer.

--- kascade/reconstruction_efficiency.py
+++ kascade/reconstruction_efficiency.py
@@ -6,6 +6,10 @@ from scipy import optimize
 from sapphire.analysis import landau
 
 
+RANGE_MAX = 40000
+N_BINS = 400
+
+
 def main():
     plot_landau_fit()
 
@@ -15,7 +19,7 @@ def plot_landau_fit():
     events = data.root.hisparc.cluster_kascade.station_601.events
     ph0 = events.col('integrals')[:, 0]
 
-    bins = np.linspace(0, 20000, 101)
+    bins = np.linspace(0, RANGE_MAX, N_BINS + 1)
     n, bins = np.histogram(ph0, bins=bins)
     x = (bins[:-1] + bins[1:]) / 2
 
@@ -35,7 +39,7 @@ def plot_landau_and_gamma(x, p_gamma, p_landau):
     gammas = gamma_func(x, *p_gamma)
     plot(x, gammas)
 
-    nx = linspace(-20000, 20000, 201)
+    nx = linspace(-RANGE_MAX, RANGE_MAX, N_BINS * 2 + 1)
     nlandaus = scintillator.conv_landau(nx, *p_landau)
     landaus = interp(x, nx, nlandaus)
     plot(x, landaus)
@@ -52,20 +56,33 @@ def fit_gammas_to_data(x, y):
 gamma_func = lambda x, N, a: N * x ** -a
 
 def fit_conv_landau_to_data(x, y, p0=(1e4 / .32, 3.38 / 5000, 1)):
-    x_symm = np.linspace(-20000, 20000, 201)
+    x_symm = np.linspace(-RANGE_MAX, RANGE_MAX, N_BINS * 2 + 1)
     y_symm = np.interp(x_symm, x, y)
     popt = optimize.fmin(scintillator.residuals, p0,
                          (x_symm, y_symm, 4500, 5500))
     return popt
 
 def fit_complete(x, y, p_gamma, p_landau):
-    x_symm = np.linspace(-20000, 20000, 201)
+    x_symm = np.linspace(-RANGE_MAX, RANGE_MAX, N_BINS * 2 + 1)
     y_symm = np.interp(x_symm, x, y)
     p0 = list(p_gamma) + list(p_landau)
     popt = optimize.fmin(complete_residuals, p0,
                          (scintillator, x_symm, y_symm, 500, 6000),
                          maxfun=100000)
-    print popt
+    print p0
+    print popt, '%e' % complete_residuals(popt, scintillator, x_symm, y_symm, 500, 6000)
+    print [u / v for u, v in zip(p0, popt)]
+    bounds = [(0.7 * u, 1.3 * u) for u in p0]
+    popt, n, r = optimize.fmin_tnc(complete_residuals, p0, bounds=bounds,
+                                        approx_grad=True, maxfun=10000,
+                                        args=(scintillator,
+                                              x_symm, y_symm, 500, 6000))
+    print popt, '%e' % complete_residuals(popt, scintillator, x_symm, y_symm, 500, 6000)
+    print n
+
+    print
+    print
+    print [u / v for u, v in zip(p0, popt)]
     return popt[:2], popt[2:]
 
 def complete_residuals(par, scintillator, x, y, a, b):
@@ -78,6 +95,8 @@ def complete_residuals(par, scintillator, x, y, a, b):
 
 
 if __name__ == '__main__':
+    np.seterr(invalid='ignore', divide='ignore')
+
     if 'data' not in globals():
         data = tables.openFile('kascade.h5', 'r')
     if 'scintillator' not in globals():
