9f9a6c4e364712e798a122842f4be2d3ec97e26f
David Fokkema <davidf@nikhef.nl>
2012-01-21 16:28:40 +0100

    Added custom constrained optimizer
    First, call the full-spectrum optimizer.  Then, for selections of events,
    call the constrained optimizer to determine the count scales for the gamma
    and landau/gauss parts of the spectrum, independently.

--- kascade/reconstruction_efficiency.py
+++ kascade/reconstruction_efficiency.py
@@ -18,6 +18,17 @@ class ReconstructionEfficiency(object):
     def main(self):
         self.plot_landau_fit()
 
+    def full_spectrum_fit(self, x, y, p0_gamma, p0_landau):
+        p_gamma = self.fit_gammas_to_data(x, y, p0_gamma)
+        p_landau = self.fit_conv_landau_to_data(x, y - self.gamma_func(x, *p_gamma),
+                                                p0_landau)
+        p_gamma, p_landau = self.fit_complete(x, y, p_gamma, p_landau)
+        return p_gamma, p_landau
+
+    def constrained_full_spectrum_fit(self, x, y, p0_gamma, p0_landau):
+        p_gamma, p_landau = self.constrained_fit_complete(x, y, p0_gamma, p0_landau)
+        return p_gamma, p_landau
+
     def plot_landau_fit(self):
         global x, n, bins, p_gamma, p_landau
 
@@ -27,10 +38,16 @@ class ReconstructionEfficiency(object):
         bins = np.linspace(0, RANGE_MAX, N_BINS + 1)
         n, bins = np.histogram(ph0, bins=bins)
         x = (bins[:-1] + bins[1:]) / 2
+    
+        p_gamma, p_landau = self.full_spectrum_fit(x, n, (1., 1.),
+                                                   (5e3 / .32, 3.38 / 5000, 1.))
+        print "FULL FIT"
+        print p_gamma, p_landau
 
-        p_gamma = self.fit_gammas_to_data(x, n)
-        p_landau = self.fit_conv_landau_to_data(x, n - self.gamma_func(x, *p_gamma))
-        p_gamma, p_landau = self.fit_complete(x, n, p_gamma, p_landau)
+        n /= 10
+        p_gamma, p_landau = self.constrained_full_spectrum_fit(x, n, p_gamma, p_landau)
+        print "CONSTRAINED FIT"
+        print p_gamma, p_landau
 
         clf()
         plt.plot(x, n)
@@ -38,7 +55,7 @@ class ReconstructionEfficiency(object):
         plt.plot(x, n - self.gamma_func(x, *p_gamma))
         plt.yscale('log')
         plt.xlim(xmin=0)
-        plt.ylim(ymin=1e2)
+        plt.ylim(ymin=1e1)
 
     def plot_landau_and_gamma(self, x, p_gamma, p_landau):
         gammas = self.gamma_func(x, *p_gamma)
@@ -52,17 +69,17 @@ class ReconstructionEfficiency(object):
         plt.plot(x, gammas + landaus)
 
 
-    def fit_gammas_to_data(self, x, y):
+    def fit_gammas_to_data(self, x, y, p0):
         condition = (500 <= x) & (x < 2000)
         x_trunc = x.compress(condition)
         y_trunc = y.compress(condition)
-        popt, pcov = optimize.curve_fit(self.gamma_func, x_trunc, y_trunc, p0=(1., 1.))
+        popt, pcov = optimize.curve_fit(self.gamma_func, x_trunc, y_trunc, p0=p0)
         return popt
 
     def gamma_func(self, x, N, a):
         return N * x ** -a
 
-    def fit_conv_landau_to_data(self, x, y, p0=(1e4 / .32, 3.38 / 5000, 1)):
+    def fit_conv_landau_to_data(self, x, y, p0):
         x_symm = np.linspace(-RANGE_MAX, RANGE_MAX, N_BINS * 2 + 1)
         y_symm = np.interp(x_symm, x, y)
         popt = optimize.fmin(self.scintillator.residuals, p0,
@@ -78,6 +95,20 @@ class ReconstructionEfficiency(object):
                              maxfun=100000)
         return popt[:2], popt[2:]
 
+    def constrained_fit_complete(self, x, y, p_gamma, p_landau):
+        x_symm = np.linspace(-RANGE_MAX, RANGE_MAX, N_BINS * 2 + 1)
+        y_symm = np.interp(x_symm, x, y)
+        N_gamma = p_gamma[0]
+        N_landau = p_landau[0]
+        popt = optimize.fmin(self.constrained_complete_residuals,
+                             (N_gamma, N_landau),
+                             (self.scintillator, x_symm, y_symm, p_gamma,
+                              p_landau, 500, 6000),
+                             maxfun=100000)
+        p_gamma[0] = popt[0]
+        p_landau[0] = popt[1]
+        return p_gamma, p_landau
+
     def complete_residuals(self, par, scintillator, x, y, a, b):
         landaus = scintillator.conv_landau(x, *par[2:])
         gammas = self.gamma_func(x, *par[:2])
@@ -85,6 +116,11 @@ class ReconstructionEfficiency(object):
         residuals = residuals.compress((a <= x) & (x < b))
         residuals = residuals.sum()
         return residuals
+
+    def constrained_complete_residuals(self, par, scintillator, x, y,
+                                       p_gamma, p_landau, a, b):
+        full_par = (par[0], p_gamma[1], par[1], p_landau[1], p_landau[2])
+        return self.complete_residuals(full_par, scintillator, x, y, a, b)
 
 
 if __name__ == '__main__':
