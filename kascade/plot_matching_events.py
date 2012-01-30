0a2ba6b51d9f7a8ee5ecd1d414d97a9b417a32b9
David Fokkema <davidf@nikhef.nl>
2012-01-30 15:50:28 +0100

    Nicer matched events timing plots

--- kascade/plot_matching_events.py
+++ kascade/plot_matching_events.py
@@ -25,7 +25,7 @@ if USE_TEX:
 
 
 def do_matching_plots(data):
-    plot_nearest_neighbors(data, limit=10000)
+    plot_nearest_neighbors(data)
     plot_residual_time_differences(data)
 
 def plot_nearest_neighbors(data, limit=None):
@@ -73,8 +73,8 @@ def plot_residual_time_differences(data):
     events = data.root.kascade.events
     c_index = data.root.kascade.c_index
 
-    t0 = make_timestamp(2008, 6, 4)
-    t1 = make_timestamp(2008, 8, 4, 1)
+    t0 = make_timestamp(2008, 7, 2)
+    t1 = make_timestamp(2008, 7, 3)
 
     idxes = events.getWhereList('(t0 <= timestamp) & (timestamp < t1)')
     t0_idx = min(idxes)
@@ -82,11 +82,19 @@ def plot_residual_time_differences(data):
 
     dts = c_index.readWhere('(t0_idx <= k_idx) & (k_idx < t1_idx)',
                             field='dt')
+    all_dts = c_index.col('dt')
 
     figure()
-    hist(dts / 1e3, bins=200, histtype='step')
-    xlabel("Time difference [ns]")
+    subplot(121)
+    hist(all_dts / 1e3, bins=arange(-10, 2, .01), histtype='step')
+    title("July 1 - Aug 6, 2008")
+    xlabel("Time difference [us]")
     ylabel("Counts")
+
+    subplot(122)
+    hist(dts / 1e3, bins=arange(-8, -6, .01), histtype='step')
+    title("July 2, 2008")
+    xlabel("Time difference [us]")
     utils.saveplot()
 
 def make_timestamp(year, month, day, hour=0, minutes=0, seconds=0):
@@ -100,7 +108,7 @@ if __name__ == '__main__':
     try:
         data
     except NameError:
-        data = tables.openFile('kascade-small.h5', 'r')
+        data = tables.openFile('kascade.h5', 'r')
 
     utils.set_prefix('MAT')
     do_matching_plots(data)
