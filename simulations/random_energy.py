bcd283736ea5f9b405d270c99a3b3cfdae849cb1
David Fokkema <davidf@nikhef.nl>
2012-02-27 17:09:57 +0100

    Random generator for primary energies
    Approximates the flux by F(E) ~ E ** -2.7.

---
+++ simulations/random_energy.py
@@ -0,0 +1,21 @@
+flux = lambda x: x ** -2.7
+
+def random_energy(a, b, size=1):
+    y0 = flux(a)
+    y1 = flux(b)
+
+    energies = []
+    for i in range(size):
+        while True:
+            x = random.uniform(a, b)
+            y = random.uniform(y0, y1)
+
+            if y <= flux(x):
+                break
+
+        energies.append(x)
+
+    if size == 1:
+        return energies[0]
+    else:
+        return array(energies)
