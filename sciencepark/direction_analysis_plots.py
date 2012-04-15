27bb463820cc5d87255b21c1e6fbf0ad6b2600db
David Fokkema <davidf@nikhef.nl>
2012-04-15 16:22:44 +0200

    Plots: single vs cluster reconstruction

--- sciencepark/direction_analysis_plots.py
+++ sciencepark/direction_analysis_plots.py
@@ -1,11 +1,30 @@
+import itertools
+
 import tables
 from pylab import *
 
 import master
+import utils
 
 
-def main():
+USE_TEX = False
+
+# For matplotlib plots
+if USE_TEX:
+    rcParams['font.serif'] = 'Computer Modern'
+    rcParams['font.sans-serif'] = 'Computer Modern'
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['figure.figsize'] = [4 * x for x in (1, 2. / 3)]
+    rcParams['figure.subplot.left'] = 0.175
+    rcParams['figure.subplot.bottom'] = 0.175
+    rcParams['font.size'] = 10
+    rcParams['legend.fontsize'] = 'small'
+    rcParams['text.usetex'] = True
+
+
+def main(data):
     plot_sciencepark_cluster()
+    plot_all_single_and_cluster_combinations(data)
 
 def plot_sciencepark_cluster():
     cluster = master.ScienceParkCluster(range(501, 507))
@@ -20,9 +39,58 @@ def plot_sciencepark_cluster():
             scatter(x, y, c='black', s=3)
     axis('equal')
 
-    data = array([xl, yl]).T
-    savetxt('plots/sciencepark-locations.txt', data)
+    utils.savedata([xl, yl])
+
+def plot_all_single_and_cluster_combinations(data):
+    for station_group in itertools.combinations(range(501, 507), 3):
+        for station in station_group:
+            plot_direction_single_vs_cluster(data, station, station_group)
+
+def plot_direction_single_vs_cluster(data, station, cluster):
+    reconstructions = data.root.reconstructions.reconstructions
+
+    station_query = '(N == 1) & s%d' % station
+    cluster_query = '(N == 3) & ' + ' & '.join(['s%d' % u for u in cluster])
+    cluster_str = [str(u) for u in cluster]
+
+    sel_station = reconstructions.readWhere(station_query)
+    sel_cluster = reconstructions.readWhere(cluster_query)
+
+    theta_cluster, theta_station = [], []
+    phi_cluster, phi_station = [], []
+    for event_cluster in sel_cluster:
+        coinc_id = event_cluster['coinc_id']
+        event_station = sel_station.compress(sel_station[:]['coinc_id'] == coinc_id)
+        assert len(event_station) <= 1
+        if event_station:
+            theta_cluster.append(event_cluster['reconstructed_theta'])
+            phi_cluster.append(event_cluster['reconstructed_phi'])
+            theta_station.append(event_station['reconstructed_theta'])
+            phi_station.append(event_station['reconstructed_phi'])
+
+    figsize = list(rcParams['figure.figsize'])
+    figsize[1] = figsize[0] / 2
+    figure(figsize=figsize)
+    subplot(121)
+    plot(theta_station, theta_cluster, ',')
+    xlabel(r"$\theta_{%d}$" % station)
+    ylabel(r"$\theta_{\{%s\}}$" % ','.join(cluster_str))
+    xlim(0, pi / 2)
+    ylim(0, pi / 2)
+
+    subplot(122)
+    plot(phi_station, phi_cluster, ',')
+    xlabel(r"$\phi_{%d}$" % station)
+    ylabel(r"$\phi_{\{%s\}}$" % ','.join(cluster_str))
+    xlim(-pi, pi)
+    ylim(-pi, pi)
+
+    utils.saveplot('%d-%s' % (station, '_'.join(cluster_str)))
 
 
 if __name__ == '__main__':
-    main()
+    if 'data' not in globals():
+        data = tables.openFile('master.h5')
+
+    utils.set_prefix("SP-DIR-")
+    main(data)
---
+++ sciencepark/utils.py
@@ -0,0 +1,43 @@
+""" Utility functions """
+
+import inspect
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+__suffix = ''
+__prefix = ''
+
+
+def set_suffix(suffix):
+    global __suffix
+    __suffix = suffix
+
+def set_prefix(prefix):
+    global __prefix
+    __prefix = prefix
+
+def whosparent():
+    """Return parent function name of caller"""
+
+    return inspect.stack()[2][3]
+
+def saveplot(suffix=''):
+    """Save a plot using caller's name"""
+
+    if suffix:
+        suffix = '-%s' % suffix
+    plt.savefig('plots/%s%s%s%s.pdf' % (__prefix, whosparent(), suffix, __suffix))
+
+def savedata(data, suffix=''):
+    """Save a plot using caller's name"""
+
+    if suffix:
+        suffix = '-%s' % suffix
+    filename = 'plots/%s%s%s%s.txt' % (__prefix, whosparent(), suffix, __suffix)
+    np.savetxt(filename, data)
+
+def title(text):
+    plt.title(text + '\n(%s)' % __suffix)
+
+mylog = np.vectorize(lambda x: np.log10(x) if x > 0 else 0)
