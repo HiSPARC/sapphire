7d80d6a0867fcf020ff9e91135d1227811796c9e
David Fokkema <davidf@nikhef.nl>
2012-05-19 22:32:28 +0200

    Cleaned up single station histograms

--- sciencepark/direction_analysis_plots.py
+++ sciencepark/direction_analysis_plots.py
@@ -35,11 +35,11 @@ def main(data):
     #plot_all_single_and_cluster_combinations(data)
     #hist_phi_single_stations(data)
     #hist_theta_single_stations(data)
-    plot_N_vs_R(data)
-    plot_fav_single_vs_cluster(data)
+    #plot_N_vs_R(data)
+    #plot_fav_single_vs_cluster(data)
     plot_fav_single_vs_single(data)
-    plot_fav_uncertainty_single_vs_cluster(data)
-    plot_fav_uncertainty_single_vs_single(data)
+    #plot_fav_uncertainty_single_vs_cluster(data)
+    #plot_fav_uncertainty_single_vs_single(data)
     #hist_fav_single_stations(data)
 
 def plot_sciencepark_cluster():
@@ -513,19 +513,22 @@ def hist_fav_single_stations(data):
         xlabel(r"$\phi$")
         legend([station], loc='lower right')
         locator_params(tight=True, nbins=4)
+        axis('auto')
 
         subplot(2, 3, n + 3)
         hist(rad2deg(theta), bins=linspace(0, 45, 21), histtype='step')
         xlabel(r"$\theta$")
         legend([station], loc='lower right')
         locator_params(tight=True, nbins=4)
+        axis('auto')
 
+    subplots_adjust(wspace=.4)
     utils.saveplot()
 
 
 if __name__ == '__main__':
     if 'data' not in globals():
-        data = tables.openFile('large.h5')
+        data = tables.openFile('month-single.h5')
 
     utils.set_prefix("SP-DIR-")
     main(data)
