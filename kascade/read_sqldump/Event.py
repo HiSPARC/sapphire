2ca5eed54bd2a8a1b9d60b6182aa11f9d59a1de2
Arne de Laat <adelaat@nikhef.nl>
2012-11-20 09:59:11 +0100

    PEP8 - E101, W191 tabs!

--- kascade/read_sqldump/Event.py
+++ kascade/read_sqldump/Event.py
@@ -1,5 +1,5 @@
 """
-	This module creates different types of Events that are specified by the subclasses
+    This module creates different types of Events that are specified by the subclasses
 """
 
 __author__ = "thevinh"
@@ -7,19 +7,19 @@ __date__ = "$16-sep-2009"
 
 
 class Event():
-	# the instantiation operation
-	def __init__(self):
-		# init variables here if needed
-		self.datetime = 0
-		self.uploadCode = 0
-		self.data = 0
-		self.nanoseconds = 0
-		self.export_values = 0
+    # the instantiation operation
+    def __init__(self):
+        # init variables here if needed
+        self.datetime = 0
+        self.uploadCode = 0
+        self.data = 0
+        self.nanoseconds = 0
+        self.export_values = 0
 
-	#--------------------------End of __init__--------------------------#
+    #--------------------------End of __init__--------------------------#
 
-	def getEventData(self):
-		pass
+    def getEventData(self):
+        pass
 
-	def parseMessage(self):
-		pass
+    def parseMessage(self):
+        pass
