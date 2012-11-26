c6597d90fb2c9fd83d1b1c455205b7b13856f017
David Fokkema <davidfokkema@gmail.com>
2012-11-26 15:34:33 +0100

    Added SAPPHiRE docstring

--- sapphire/__init__.py
+++ sapphire/__init__.py
@@ -1,1 +1,34 @@
-"""Simulation and Analysis Program Package for HiSPARC Research"""
+"""Simulation and Analysis Program Package for HiSPARC Research
+
+SAPPHiRE simplifies data access, simulations and analysis for the
+`HiSPARC <http://www.hisparc.nl>`_ experiment.  It was born out of a
+combination of the old framework and the collection of simulation and
+analysis scripts developed by David Fokkema for his PhD thesis work.
+
+The following packages and modules are included:
+
+:mod:`~sapphire.analysis`
+    package containing analysis-related modules
+
+:mod:`~sapphire.clusters`
+    definitions for HiSPARC detectors, stations and clusters
+
+:mod:`~sapphire.gpstime`
+    conversion functions for converting GPS time to UTC and vice versa
+
+:mod:`~sapphire.kascade`
+    work on KASCADE data
+
+:mod:`~sapphire.publicdb`
+    public data access
+
+:mod:`~sapphire.simulations`
+    package containing simulation-related modules
+
+:mod:`~sapphire.storage`
+    storage-related definitions
+
+:mod:`~sapphire.transformations`
+    geographic coordinate transformations
+
+"""
