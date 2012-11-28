2eaeb2e51b97501ad098f8f40791702b3563943d
David Fokkema <davidfokkema@gmail.com>
2012-11-28 13:42:31 +0100

    Setup.py check now runs cleanly
    I've tested the following commands successfully:
    
        $ python setup.py build
        $ python setup.py install
        $ python setup.py build_sphinx

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ setup(
     name = "SAPPHiRE",
     version = "0.9.1b",
     packages = find_packages(),
+    url = "http://github.com/hisparc/sapphire/",
+    author = "David Fokkema",
+    author_email = "davidf@nikhef.nl",
 )
