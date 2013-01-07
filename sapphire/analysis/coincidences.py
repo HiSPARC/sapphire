06b10b80c75c49dc7d2f4d4d0d784acbbf83a49b
David Fokkema <davidfokkema@gmail.com>
2013-01-07 15:30:28 +0100

    WIP: moved two functions to class methods

--- sapphire/analysis/coincidences.py
+++ sapphire/analysis/coincidences.py
@@ -99,94 +99,92 @@ class Coincidences:
                 if shifts[i]:
                     shifts[i] = long(shifts[i] * 1e9)
 
-        timestamps = retrieve_timestamps(stations, shifts, limit)
-        coincidences = do_search_coincidences(timestamps, window)
+        timestamps = self._retrieve_timestamps(stations, shifts, limit)
+        coincidences = self._do_search_coincidences(timestamps, window)
 
         return coincidences, timestamps
 
+    def _retrieve_timestamps(self, stations, shifts=None, limit=None):
+        """Retrieve all timestamps from all stations, optionally shifting them
 
-def retrieve_timestamps(stations, shifts=None, limit=None):
-    """Retrieve all timestamps from all stations, optionally shifting them
+        This function retrieves the timestamps from a list of event tables and
+        will optionally shift the timestamps by a given amount.  This is
+        necessary when one wants to compare the timestamps of stations who use
+        a different time (as in GPS, UTC or local time).
 
-    This function retrieves the timestamps from a list of event tables and
-    will optionally shift the timestamps by a given amount.  This is
-    necessary when one wants to compare the timestamps of stations who use
-    a different time (as in GPS, UTC or local time).
+        :param stations: a list of HiSPARC event tables (normally from
+            different stations, hence the name)
+        :param shifts: a list of time shifts which may contain 'None'.
+        :param limit: limit the number of events which are processed
 
-    :param stations: a list of HiSPARC event tables (normally from
-        different stations, hence the name)
-    :param shifts: a list of time shifts which may contain 'None'.
-    :param limit: limit the number of events which are processed
+        :return: list of tuples.  Each tuple consists of a timestamp followed
+            by an index into the stations list which designates the detector
+            station which measured the event, and finally an index of the
+            event into the station's event table.
 
-    :return: list of tuples.  Each tuple consists of a timestamp followed
-        by an index into the stations list which designates the detector
-        station which measured the event, and finally an index of the
-        event into the station's event table.
+        """
+        timestamps = []
+        for i in range(len(stations)):
+            ts = [(x['ext_timestamp'], i, j) for j, x in
+                  enumerate(stations[i][:limit])]
+            try:
+                # shift data. carefully avoid upcasting (we're adding two
+                # longs, which is a long, and casting that back to uint64. if
+                # we're not careful, an intermediate value will be a float64,
+                # which doesn't hold the precision to store nanoseconds.
+                ts = [(np.uint64(long(x[0]) + shifts[i]), x[1], x[2]) for x in
+                      ts]
+            except (TypeError, IndexError):
+                # shift is None or doesn't exist
+                pass
+            timestamps.extend(ts)
 
-    """
-    timestamps = []
-    for i in range(len(stations)):
-        ts = [(x['ext_timestamp'], i, j) for j, x in
-              enumerate(stations[i][:limit])]
-        try:
-            # shift data. carefully avoid upcasting (we're adding two
-            # longs, which is a long, and casting that back to uint64. if
-            # we're not careful, an intermediate value will be a float64,
-            # which doesn't hold the precision to store nanoseconds.
-            ts = [(np.uint64(long(x[0]) + shifts[i]), x[1], x[2]) for x in
-                  ts]
-        except (TypeError, IndexError):
-            # shift is None or doesn't exist
-            pass
-        timestamps.extend(ts)
+        # sort the timestamps
+        timestamps.sort()
 
-    # sort the timestamps
-    timestamps.sort()
+        return timestamps
 
-    return timestamps
+    def _do_search_coincidences(self, timestamps, window):
+        """Search for coincidences in a set of timestamps
 
+        Given a set of timestamps, search for coincidences.  That is, search
+        for events which occured almost at the same time and thus might be the
+        result of an extended air shower.
 
-def do_search_coincidences(timestamps, window=200000):
-    """Search for coincidences in a set of timestamps
+        :param timestamps: a list of tuples (timestamp, station_idx,
+            event_idx) which will be searched
+        :param window: the time window in nanoseconds which will be searched
+            for coincidences.  Events falling outside this window will not be
+            part of the coincidence.  Default: 200000 (i.e. 200 us).
 
-    Given a set of timestamps, search for coincidences.  That is, search
-    for events which occured almost at the same time and thus might be the
-    result of an extended air shower.
+        :return: a list of coincidences, which each consist of a list with
+            indexes into the timestamps array as a pointer to the events
+            making up the coincidence
 
-    :param timestamps: a list of tuples (timestamp, station_idx,
-        event_idx) which will be searched
-    :param window: the time window in nanoseconds which will be searched
-        for coincidences.  Events falling outside this window will not be
-        part of the coincidence.  Default: 200000 (i.e. 200 us).
+        """
+        coincidences = []
 
-    :return: a list of coincidences, which each consist of a list with
-        indexes into the timestamps array as a pointer to the events
-        making up the coincidence
+        # traverse all timestamps
+        for i in xrange(len(timestamps)):
 
-    """
-    coincidences = []
+            # build coincidence, starting with the current timestamp
+            c = [i]
+            t0 = timestamps[i][0]
 
-    # traverse all timestamps
-    for i in xrange(len(timestamps)):
+            # traverse the rest of the timestamps
+            for j in xrange(i + 1, len(timestamps)):
+                # if a timestamp is within the coincidence window, add it
+                if timestamps[j][0] - t0 < window:
+                    c.append(j)
+                else:
+                    # coincidence window has passed, break for-loop
+                    break
 
-        # build coincidence, starting with the current timestamp
-        c = [i]
-        t0 = timestamps[i][0]
+            # if we have more than one event in the coincidence, save it
+            if len(c) > 1:
+                coincidences.append(c)
 
-        # traverse the rest of the timestamps
-        for j in xrange(i + 1, len(timestamps)):
-            # if a timestamp is within the coincidence window, add it
-            if timestamps[j][0] - t0 < window:
-                c.append(j)
-            else:
-                # coincidence window has passed, break for-loop
-                break
-
-        # if we have more than one event in the coincidence, save it
-        if len(c) > 1:
-            coincidences.append(c)
-
-    return coincidences
+        return coincidences
 
 
 def get_events(data, stations, coincidence, timestamps,
