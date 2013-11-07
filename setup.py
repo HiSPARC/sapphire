2cb85703062571a766ecd0ee1c970b0d9ffa89f6
Arne de Laat <adelaat@nikhef.nl>
2013-11-07 16:21:18 +0100

    Update for PyPi
    Update setup.py for PyPi
    Update README.rst with new instructions
    (todo: also change instructions in /doc)

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
-    name = "sapphire",
-    version = "0.9.2b",
+    name = "hisparc-sapphire",
+    version = "0.9.3",
     packages = find_packages(),
     url = "http://github.com/hisparc/sapphire/",
+    license = "GPLv3",
     author = "David Fokkema",
     author_email = "davidf@nikhef.nl",
     description = "A framework for the HiSPARC experiment",
+    long_description = long_description,
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Education',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Education'],
 
     install_requires = ['numpy', 'scipy', 'tables', 'matplotlib',
                         'progressbar', 'mock'],
