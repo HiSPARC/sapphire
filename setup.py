73b3c9e57f2e511bee7b8ecf3707aaa3818b3d27
Arne de Laat <adelaat@nikhef.nl>
2013-11-13 15:45:55 +0100

    Increment patch version to 0.9.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='hisparc-sapphire',
-      version='0.9.3',
+      version='0.9.4',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       license='GPLv3',
