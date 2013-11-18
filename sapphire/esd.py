095aba486b875a647b60c84eef616243a5c39898
David Fokkema <davidfokkema@gmail.com>
2013-11-18 16:58:05 +0100

    ESD download works as advertised

--- sapphire/esd.py
+++ sapphire/esd.py
@@ -8,8 +8,7 @@
 import urllib2
 import urllib
 import csv
-import StringIO
-from numpy import genfromtxt
+import os.path
 
 import tables
 
@@ -17,7 +16,7 @@ import tables
 URL = 'http://data.hisparc.nl/data/%d/events'
 
 
-def download_data(file, group, station_id, start, end, func):
+def download_data(file, group, station_id, start, end):
     """Download event summary data
 
     :param file: The PyTables datafile handler
@@ -33,8 +32,9 @@ def download_data(file, group, station_id, start, end, func):
         >>> import tables
         >>> import datetime
         >>> import sapphire.esd
-        >>> data = tables.openFile('data.h5', 'w')
-        >>> sapphire.esd.download_data(data, '/s501', 501, datetime.datetime(2010, 9, 1), datetime.datetime(2010, 9, 2))
+        >>> data = tables.open_file('data.h5', 'w')
+        >>> sapphire.esd.download_data(data, '/s501', 501,
+        ... datetime.datetime(2013, 9, 1), datetime.datetime(2013, 9, 2))
 
     """
     url = URL % station_id
@@ -43,13 +43,9 @@ def download_data(file, group, station_id, start, end, func):
 
     data = urllib2.urlopen(url)
 
-    func(file, group, data)
-
-
-def parse1(file, group, data):
     table = create_table(file, group)
-    reader = csv.reader(data, delimiter='\t')
 
+    reader = csv.reader(data, delimiter='\t')
     for line in reader:
         if line[0][0] == '#':
             continue
@@ -75,48 +71,6 @@ def parse1(file, group, data):
                        t3, t4]])
 
 
-def parse2(file, group, data):
-    table = create_table(file, group)
-
-    format = [('date', 'datetime64[D]'), ('time', '|S8'),
-              ('timestamp', 'uint32'), ('nanoseconds', 'uint32'),
-              ('pulseheights', '4int16'), ('integrals', '4int16'),
-              ('n1', 'float32'), ('n2', 'float32'),
-              ('n3', 'float32'), ('n4', 'float32'),
-              ('t1', 'float32'), ('t2', 'float32'),
-              ('t3', 'float32'), ('t4', 'float32')]
-
-
-    for line in data:
-        try:
-            x = genfromtxt(StringIO.StringIO(line), delimiter='\t', dtype=format)
-        except IOError:
-            continue
-
-        x = x.tolist()
-        #table.append([[x[2], x[3], x[2] * int(1e9) + x[3], x[4], x[5],
-        #               x[6], x[7], x[8], x[9], x[10], x[11], x[12],
-        #               x[13]]])
-        timestamp = x[2]
-        nanoseconds = x[3]
-        ext_timestamp = timestamp * int(1e9) + nanoseconds
-        pulseheights = list(x[4])
-        integrals = list(x[5])
-        n1 = x[6]
-        n2 = x[7]
-        n3 = x[8]
-        n4 = x[9]
-        t1 = x[10]
-        t2 = x[11]
-        t3 = x[12]
-        t4 = x[13]
-        #print timestamp, nanoseconds, ext_timestamp, pulseheights, \
-        #    integrals, n1, n2, n3, n4, t1, t2, t3, t4
-        table.append([[timestamp, nanoseconds, ext_timestamp,
-                       pulseheights, integrals, n1, n2, n3, n4, t1, t2,
-                       t3, t4]])
-
-
 def create_table(file, group):
     description = {'timestamp': tables.Time32Col(pos=0),
                    'nanoseconds': tables.UInt32Col(pos=1),
@@ -131,6 +85,10 @@ def create_table(file, group):
                    't2': tables.Float32Col(pos=10),
                    't3': tables.Float32Col(pos=11),
                    't4': tables.Float32Col(pos=12)}
+
+    if group not in file:
+        head, tail = os.path.split(group)
+        file.createGroup(head, tail)
 
     if 'events' in file.getNode(group):
         file.removeNode(group, 'events')
