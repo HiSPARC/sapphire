60245d23ab30a2448e979d28a4a403984b03c0f7
Arne de Laat <adelaat@nikhef.nl>
2014-01-08 17:13:22 +0100

    Update storage of Corsika to HDF5
    Add docstrings
    Add expectedrows for more efficient hdf5 generation
    Update GroundParticles description
    Use particle id instead of particle name
    Also add hadron generation and observation level

--- sapphire/corsika/store_corsika_data.py
+++ sapphire/corsika/store_corsika_data.py
@@ -20,19 +20,25 @@ DEST_FILE = '/Users/niekschultheiss/data/data.h5'
 
 
 class GroundParticles(tables.IsDescription):
-    particle_name = tables.StringCol(30)
-    r = tables.Float32Col()
-    phi = tables.Float32Col()
-    x = tables.Float32Col()
-    y = tables.Float32Col()
-    t = tables.Float32Col()
-    p_x = tables.Float32Col()
-    p_y = tables.Float32Col()
-    p_z = tables.Float32Col()
+    """Store information about shower particles reaching ground level"""
+
+    particle_id = tables.UInt16Col(pos=0)
+    r = tables.Float32Col(pos=1)
+    phi = tables.Float32Col(pos=2)
+    x = tables.Float32Col(pos=3)
+    y = tables.Float32Col(pos=4)
+    t = tables.Float32Col(pos=5)
+    p_x = tables.Float32Col(pos=6)
+    p_y = tables.Float32Col(pos=7)
+    p_z = tables.Float32Col(pos=8)
+    hadron_generation = tables.UInt8Col(pos=9)
+    observation_level = tables.UInt8Col(pos=10)
 
 
 def save_particle(row, p):
-    row['particle_name'] = p.particle
+    """Write the information of a particle into a row"""
+
+    row['particle_id'] = p.id
     row['r'] = p.r
     row['phi'] = p.phi
     row['x'] = p.x
@@ -41,41 +47,45 @@ def save_particle(row, p):
     row['p_x'] = p.p_x
     row['p_y'] = p.p_y
     row['p_z'] = p.p_z
+    row['hadron_generation'] = p.hadron_generation
+    row['observation_level'] = p.observation_level
     row.append()
 
 
-def store_corsika_data(source, group_name, destination):
-    try:
-        table = destination.createTable('/', group_name, GroundParticles,
-                                 'All groundparticles')
-    except tables.NodeError:
-        print '%s already exists, doing nothing' % group_name
-        return
+def store_corsika_data(source, destination, table_name='groundparticles'):
+    """Store particles from a CORSIKA simulation in a HDF5 file
 
+    :param source: CorsikaFile instance of the source DAT file
+    :param destination: PyTables file instance of the destination file
+
+    """
     print "Storing CORSIKA data (%s) in %s" % (source._filename,
                                                destination.filename)
-
-    particle_row = table.row
+    source.check()
 
     for event in source.get_events():
+        n_particles = event.get_end().n_particles_levels
+        try:
+            table = destination.createTable('/', table_name, GroundParticles,
+                                            'All groundparticles',
+                                            expectedrows=n_particles)
+        except tables.NodeError:
+            print '%s already exists, doing nothing' % table_name
+            return
+        particle_row = table.row
         for particle in event.get_particles():
             save_particle(particle_row, particle)
 
     table.flush()
 
 
-def main():
+if __name__ == '__main__':
     # Source
     corsika_data = corsika.CorsikaFile(SOURCE_FILE)
-    corsika_data.check()
 
-    # Destination      
-    hdf_data = tables.openFile(DEST_FILE, 'a')
+    # Destination
+    hdf_data = tables.openFile(DEST_FILE, 'w')
 
-    store_corsika_data(corsika_data, 'groundparticles', hdf_data)
+    store_corsika_data(corsika_data, hdf_data)
 
     hdf_data.close()
-
-
-if __name__ == '__main__':
-    main()
