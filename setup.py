3117bcb98a8b9c43591673d20f92202095eb14a3
Arne de Laat <adelaat@nikhef.nl>
2014-02-26 21:45:58 +0100

    Version bump to 0.9.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='hisparc-sapphire',
-      version='0.9.5',
+      version='0.9.6',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       bugtrack_url='http://github.com/HiSPARC/sapphire/issues',
