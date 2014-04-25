fcd9ab8f963d8b0208c6a4f1e22dcdd9ce4fcdb7
Arne de Laat <adelaat@nikhef.nl>
2014-04-25 13:17:08 +0200

    Require at least progressbar 2.3
    This version allows it to be used as an iterable,
    which is functionality that we use.

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(name='hisparc-sapphire',
                                  'tests/analysis/DIR-testdata.h5',
                                  'tests/corsika/DAT000000',
                                  'tests/simulations/testdata.h5']},
-      install_requires=['numpy', 'scipy', 'tables>=3.0.0', 'progressbar',
+      install_requires=['numpy', 'scipy', 'tables>=3.0.0', 'progressbar>=2.3',
                         'mock'])
