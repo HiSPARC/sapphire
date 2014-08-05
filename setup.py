2a54b42b9c0f69e3d311e85b1f4d1904844b04fc
Arne de Laat <adelaat@nikhef.nl>
2014-08-05 11:36:41 +0200

    Bump version to 0.9.15
    Fix small bug in coincidences
    This prevented proper creation of jSparc analysis sessions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='hisparc-sapphire',
-      version='0.9.14',
+      version='0.9.15',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       bugtrack_url='http://github.com/HiSPARC/sapphire/issues',
