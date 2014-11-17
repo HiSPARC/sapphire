bd782dc0b6f949a156bab27efcde42bc134ea64d
Arne de Laat <adelaat@nikhef.nl>
2014-11-17 16:34:47 +0100

    Add test for celestial transformation
    HiSPARC zenith azimuth to Horizontal coordinates.
    Test for -π as azimuth needs results to be confined to range.

---
+++ sapphire/tests/transformations/test_celestial.py
@@ -0,0 +1,28 @@
+import unittest
+import random
+from math import pi
+
+from sapphire.transformations import celestial
+
+
+class ZenithAzimuthHorizontalTests(unittest.TestCase):
+
+    def setUp(self):
+        self.zenith = (0., pi / 4., pi / 2.)
+        self.altitude = (pi / 2., pi / 4., 0.)
+
+        self.azimuth = (-pi / 2., 0., pi / 2.)  # -pi
+        self.Azimuth = (pi, pi / 2., 0.)  # -pi / 2.
+
+    def test_zenithazimuth_to_horizontal(self):
+        for zenith, altitude in zip(self.zenith, self.altitude):
+            self.assertEqual(celestial.zenithazimuth_to_horizontal(zenith, 0)[0], altitude)
+            self.assertEqual(celestial.horizontal_to_zenithazimuth(altitude, 0)[0], zenith)
+
+        for azimuth, Azimuth in zip(self.azimuth, self.Azimuth):
+            self.assertEqual(celestial.zenithazimuth_to_horizontal(0, azimuth)[1], Azimuth)
+            self.assertEqual(celestial.horizontal_to_zenithazimuth(0, Azimuth)[1], azimuth)
+
+
+if __name__ == '__main__':
+    unittest.main()
