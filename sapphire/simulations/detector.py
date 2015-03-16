719d48c6cb2e0b6872d3dbdc5883fa704f4632a8
tomkooij <tomkooij@tomkooij.nl>
2015-03-16 15:25:36 +0100

    added better compton and pair production

--- sapphire/simulations/detector.py
+++ sapphire/simulations/detector.py
@@ -163,6 +163,7 @@ class HiSPARCSimulation(BaseSimulation):
 
         max_E = 4.0 # 2 MeV per cm * 2cm scintilator depth
         MIP = 3.38 # MeV
+        P_pair_production = 0.015
 
         # W.R. Leo (1987) p 54
         # E photon energy [MeV]
@@ -175,36 +176,102 @@ class HiSPARCSimulation(BaseSimulation):
 
             return (E * 2 * gamma / (1 + 2*gamma) )
 
-        def _energy_transfer(E):
-
+        def _compton_energy_transfer(E):
             # from lio-project/photons/electron_energy_distribution.py
 
-            # cumulative energy distribution implemented as table (this should be a matrix based on E)
-            lookup_table = [0.09673309,  0.17138467,  0.25043101,  0.33387211, 0.42170797,  0.51393859,  0.61056396,  0.7115841 ,  0.81699899, 1.0]
+            # cumulative energy distribution implemented as table
+            #   Energy, np.poly1d() fit
+            #    from lio_project/photons/electron_energy_distribution.py
+            Energy_table = np.array([\
+                     0.100000 ,\
+                     0.127427 ,\
+                     0.162378 ,\
+                     0.206914 ,\
+                     0.263665 ,\
+                     0.335982 ,\
+                     0.428133 ,\
+                     0.545559 ,\
+                     0.695193 ,\
+                     0.885867 ,\
+                     1.128838 ,\
+                     1.438450 ,\
+                     1.832981 ,\
+                     2.335721 ,\
+                     2.976351 ,\
+                     3.792690 ,\
+                     4.832930 ,\
+                     6.158482 ,\
+                     7.847600 ,\
+                     10.000000])
 
-            return lookup_table[np.random.randint(10)]*_compton_edge(E)
+            transfer_function_table = [\
+                    [-0.095663 , 0.998190 , 0.042602],\
+                    [-0.104635 , 1.008633 , 0.040506],\
+                    [-0.109294 , 1.015132 , 0.038090],\
+                    [-0.107136 , 1.015115 , 0.035430],\
+                    [-0.095551 , 1.005781 , 0.032673],\
+                    [-0.072295 , 0.984547 , 0.030032],\
+                    [-0.036015 , 0.949579 , 0.027764],\
+                    [0.013328 , 0.900254 , 0.026124],\
+                    [0.074324 , 0.837384 , 0.025313],\
+                    [0.144294 , 0.763123 , 0.025434],\
+                    [0.219738 , 0.680594 , 0.026476],\
+                    [0.296884 , 0.593392 , 0.028324],\
+                    [0.372186 , 0.505081 , 0.030787],\
+                    [0.442678 , 0.418822 , 0.033635],\
+                    [0.506156 , 0.337141 , 0.036638],\
+                    [0.561214 , 0.261844 , 0.039593],\
+                    [0.607175 , 0.194044 , 0.042338],\
+                    [0.643960 , 0.134252 , 0.044755],\
+                    [0.671940 , 0.082499 , 0.046776],\
+                    [0.691794 , 0.038463 , 0.048368],
+                    [0.691794 , 0.038463 , 0.048368]]  # extra item E > 10
 
+            idx = Energy_table.searchsorted(E, side='left')
+            p = np.poly1d(transfer_function_table[idx])
+            return p(np.random.random())*_compton_edge(E)
 
-        # p [eV]
-        # E [MeV]
-        E = p / 1.e6
-        k = np.random.random(n)
-        interaction_probability = 0.134198 * np.exp(-0.392398*E) + 0.034156
+        def _compton_interaction_probability(E):
+            return 0.134198 * np.exp(-0.392398*E) + 0.034156
 
-        photons = zip(E, k, interaction_probability)
+        #p [eV]
+        #E [MeV]
+        #E = p / 1.e6
+        #k = np.random.random(n)
+        #interaction_probability = 0.134198 * np.exp(-0.392398*E) + 0.034156
+
+        n = 3
+        E = np.random.rand(100)*10.    # DIT ZIJN ALTIJD LIJSTEN!
 
         mips = 0
-        for photon in photons:
+        for energy in E:
 
-            if photon[1] < photon[2]: #   (k < interaction_probability)
-                maximum_energy_deposit_in_MIPS = (1-photon[1]/photon[2])*max_E/MIP
-                energy_deposit_in_MIPS = _energy_transfer(photon[0])/max_E
+            depth_compton = np.random.random()/_compton_interaction_probability(energy) # unit: scintilator depth
+            depth_pair = np.random.random()/P_pair_production # 0.015 = P(pair production)
+            #print "E, depth compton, pair = ", energy, depth_compton, depth_pair
 
+            if ((depth_pair > 1.0) & (depth_compton > 1.0)):
+                continue
+
+            #  interaction in scintilator
+            if ((energy < 1.022) | (depth_compton < depth_pair)):
+                # compton scattering
+                maximum_energy_deposit_in_MIPS = (1-depth_compton)*max_E/MIP
+                energy_deposit_in_MIPS = _compton_energy_transfer(energy)/MIP
                 extra_mips = np.minimum(maximum_energy_deposit_in_MIPS, energy_deposit_in_MIPS)  # maximise energy transfer per photon to 1 MIP/cm * depth
                 # stdout output: Energy, k, interaction_probability, transfered_energy [mips]
                 # print '*', photon[0], photon[1], photon[2], extra_mips
                 mips += extra_mips
+                print "compton: ", extra_mips
+            else:
+                # pair production: Two "electrons"
+                maximum_energy_deposit_in_MIPS = (1-depth_pair)*max_E/MIP
+                energy_deposit_in_MIPS = (energy - 1.022) / MIP # 1.022 MeV used for creation of two particles
+                extra_mips = np.minimum(maximum_energy_deposit_in_MIPS, energy_deposit_in_MIPS)
+                mips += extra_mips
+                print "pair! ",extra_mips
 
+        print "mips = ", mips
         return mips
 
     @classmethod
