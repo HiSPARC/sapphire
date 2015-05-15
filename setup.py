d4ad5d4bd947c284957d7b50a424a5ebacaab117
Arne de Laat <adelaat@nikhef.nl>
2015-05-15 08:52:59 +0200

    Bump version to v0.10.1
    Add round in base function.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='hisparc-sapphire',
-      version='0.10.0',
+      version='0.10.1',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       bugtrack_url='http://github.com/HiSPARC/sapphire/issues',
