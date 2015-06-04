7c0bd71b5b80ea74ab090caa1dafb2cdf706bfea
tomkooij <tomkooij@tomkooij.nl>
2015-06-04 16:26:18 +0200

    change 01 (octal) to 1 (integer)

--- sapphire/tests/esd_load_data.py
+++ sapphire/tests/esd_load_data.py
@@ -36,8 +36,8 @@ def perform_esd_download_data(filename):
     """Load data from esd/api to h5 (filename)"""
 
     filters = tables.Filters(complevel=1)
-    start = datetime.datetime(2012, 01, 01, 0, 0, 0)
-    eind = datetime.datetime(2012, 01, 01, 0, 1, 0)
+    start = datetime.datetime(2012, 1, 1, 0, 0, 0)
+    eind = datetime.datetime(2012, 1, 1, 0, 1, 0)
 
     with tables.open_file(filename, 'w', filters=filters) as datafile:
         esd.download_data(datafile, '/', 501, start, eind, type='events')
@@ -48,8 +48,8 @@ def perform_download_coincidences(filename):
     """Load data from esd/api to h5 (filename)"""
 
     filters = tables.Filters(complevel=1)
-    start = datetime.datetime(2012, 01, 01, 0, 0, 0)
-    eind = datetime.datetime(2012, 01, 01, 0, 2, 0)
+    start = datetime.datetime(2012, 1, 1, 0, 0, 0)
+    eind = datetime.datetime(2012, 1, 1, 0, 2, 0)
 
     with tables.open_file(filename, 'w', filters=filters) as datafile:
         esd.download_coincidences(datafile, cluster='Science Park',
