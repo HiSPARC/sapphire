fa781ca7cc01fd534d97d2763e6dd806fa2a1b0b
Arne de Laat <adelaat@nikhef.nl>
2016-01-15 18:39:54 +0100

    Fix setup.py syntax

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='hisparc-sapphire',
                                  'tests/corsika/test_data/*.h5',
                                  'tests/corsika/test_data/1_2/DAT000000',
                                  'tests/corsika/test_data/1_2/*.h5',
-                                 'tests/simulations/test_data/*.h5',
+                                 'tests/simulations/test_data/*.h5']},
       install_requires=['numpy', 'scipy', 'tables>=3.2.0', 'progressbar2',
                         'lazy', 'mock'],
       test_suite="sapphire.tests",)
