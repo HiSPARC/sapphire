7e3ef4bca13b936b0543ba93b6b2a6a16e049a42
David Fokkema <davidfokkema@gmail.com>
2016-02-08 12:03:22 +0100

    Example scripts: tips and tricks

--- doc/index.rst
+++ doc/index.rst
@@ -18,6 +18,7 @@ Contents:
 
    installation
    tutorial
+   examples
    sapphire
    README
 
---
+++ doc/scripts/is_useful.py
@@ -0,0 +1,5 @@
+def square(x):
+    return x ** 2
+
+print 1, 2, 3
+print square(1), square(2), square(3)
