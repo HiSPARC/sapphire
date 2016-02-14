e27c51f39d2e0f5bb0c83b15481a60536e9f4f98
Arne de Laat <adelaat@nikhef.nl>
2016-02-14 16:06:04 +0100

    Bump version to 0.13.2
    Enable progressbar for ReconstructESDCoincidences.
    Remove cluster keyword from coincidences store methods.
    For ESD it is replaced by a station_numbers keyword.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] != (2, 7):
 
 
 setup(name='hisparc-sapphire',
-      version='0.13.1',
+      version='0.13.2',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       bugtrack_url='http://github.com/HiSPARC/sapphire/issues',
