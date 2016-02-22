6508d0032f9b98847b9d62b2cdf12cc7d08ebd8d
David Fokkema <davidfokkema@gmail.com>
2016-02-22 16:21:42 +0100

    Fix missing script

---
+++ doc/scripts/simple-download-with-checks.py
@@ -0,0 +1,14 @@
+import datetime
+import tables
+from sapphire import esd
+
+DATAFILE = 'data.h5'
+START = datetime.datetime(2016, 1, 1)
+END = datetime.datetime(2016, 1, 2)
+
+
+if 'data' not in globals():
+    data = tables.open_file(DATAFILE, 'a')
+
+if '/s501' not in data:
+    esd.download_data(data, '/s501', 501, START, END)
