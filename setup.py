fc4d253765d35049110f1c6fc6661efc48e7647a
Arne de Laat <adelaat@nikhef.nl>
2016-03-09 16:28:51 +0100

    Bump version to 0.13.6
    Improve documentation.
    Update local data.
    Verify that downloading data is successfully completed.
    Remove MPV from station info API.
    Improve logic checks for reconstructions, relaxing some constraints.
    Improve event processing in case of external trigger or no trigger
    settings.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] != (2, 7):
 
 
 setup(name='hisparc-sapphire',
-      version='0.13.5',
+      version='0.13.6',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       bugtrack_url='http://github.com/HiSPARC/sapphire/issues',
