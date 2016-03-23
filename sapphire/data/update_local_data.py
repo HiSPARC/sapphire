f2397cebf80f252824b2e414ccbb730b44913c1c
Arne de Laat <adelaat@nikhef.nl>
2016-03-23 11:12:12 +0100

    Update local data script
    Update layout data for Twente stations.

--- sapphire/data/update_local_data.py
+++ sapphire/data/update_local_data.py
@@ -18,75 +18,86 @@ from sapphire.utils import pbar
 
 def update_local_json():
     for type in pbar(['stations', 'subclusters', 'clusters', 'countries']):
-        url = API.urls[type]
-        try:
-            data = loads(API._retrieve_url(url))
-        except:
-            print 'Failed to get %s data' % type
-            continue
-        if data:
-            json_path = path.join(LOCAL_BASE,
-                                  url.strip('/') + extsep + 'json')
-            with open(json_path, 'w') as jsonfile:
-                dump(data, jsonfile, indent=4, sort_keys=True)
+        update_toplevel_json(type)
 
-    for arg, kwarg, type in [
-            ('stations', 'station_number', 'station_info'),
-            ('subclusters', 'subcluster_number', 'stations_in_subcluster'),
-            ('clusters', 'cluster_number', 'subclusters_in_cluster'),
-            ('countries', 'country_number', 'clusters_in_country')]:
+    for arg_type, type in [('stations', 'station_info'),
+                           ('subclusters', 'stations_in_subcluster'),
+                           ('clusters', 'subclusters_in_cluster'),
+                           ('countries', 'clusters_in_country')]:
+        update_sublevel_json(arg_type, type)
+
+
+def update_toplevel_json(type):
+    url = API.urls[type]
+    try:
+        get_and_store_json(url)
+    except:
+        print 'Failed to get %s data' % type
+
+
+def update_sublevel_json(arg_type, type):
+    subdir = API.urls[type].split('/')[0]
+    try:
+        mkdir(path.join(LOCAL_BASE, subdir))
+    except OSError:
+        pass
+
+    url = API.urls[arg_type]
+    try:
+        numbers = [x['number'] for x in loads(API._retrieve_url(url))]
+    except:
+        print 'Failed to get %s data' % type
+        return
+
+    kwarg = API.urls[type].split('/')[1].strip('{}')
+    for number in pbar(numbers):
+        url = API.urls[type].format(**{kwarg: number,
+                                       'year': '', 'month': '', 'day': ''})
         try:
-            if arg == 'stations':
-                mkdir(path.join(LOCAL_BASE, arg[:-1]))
-            else:
-                mkdir(path.join(LOCAL_BASE, arg))
-        except OSError:
-            pass
-        url = API.urls[arg]
-        try:
-            numbers = [x['number'] for x in loads(API._retrieve_url(url))]
+            get_and_store_json(url.strip('/'))
         except:
-            print 'Failed to get %s data' % type
-            continue
-        for number in pbar(numbers):
-            url = API.urls[type].format(**{kwarg: number,
-                                           'year': '', 'month': '', 'day': ''})
-            try:
-                data = loads(API._retrieve_url(url.strip('/')))
-            except:
-                print 'Failed to get %s data for %s %d' % (type, arg, number)
-                continue
-            if data:
-                json_path = path.join(LOCAL_BASE,
-                                      url.strip('/') + extsep + 'json')
-                with open(json_path, 'w') as jsonfile:
-                    dump(data, jsonfile, indent=4, sort_keys=True)
+            print 'Failed to get %s data for %s %d' % (type, arg_type, number)
+            return
 
 
 def update_local_tsv():
-    """Get location tsv data for all stations"""
+    """Get configuration tsv data for all stations"""
 
     station_numbers = Network().station_numbers()
+
     for type in ['gps', 'trigger', 'layout', 'voltage', 'current']:
+        subdir = API.src_urls[type].split('/')[0]
         try:
-            mkdir(path.join(LOCAL_BASE, type))
+            mkdir(path.join(LOCAL_BASE, subdir))
         except OSError:
             pass
+
         for number in pbar(station_numbers):
             url = API.src_urls[type].format(station_number=number)
             try:
-                data = API._retrieve_url(url, base=SRC_BASE)
+                get_and_store_tsv(url)
             except:
                 if type != 'layout':
                     print 'Failed to get %s for station %d' % (type, number)
                 continue
-            data = '\n'.join(d for d in data.split('\n')
-                             if len(d) and d[0] != '#')
-            if data:
-                tsv_path = path.join(LOCAL_BASE,
-                                     url.strip('/') + extsep + 'tsv')
-                with open(tsv_path, 'w') as tsvfile:
-                    tsvfile.write(data)
+
+
+def get_and_store_json(url):
+    data = loads(API._retrieve_url(url))
+    if data:
+        json_path = path.join(LOCAL_BASE, url.strip('/') + extsep + 'json')
+        with open(json_path, 'w') as jsonfile:
+            dump(data, jsonfile, indent=4, sort_keys=True)
+
+
+def get_and_store_tsv(url):
+    data = API._retrieve_url(url, base=SRC_BASE)
+    # Strip empty and comment lines
+    data = '\n'.join(d for d in data.split('\n') if len(d) and d[0] != '#')
+    if data:
+        tsv_path = path.join(LOCAL_BASE, url.strip('/') + extsep + 'tsv')
+        with open(tsv_path, 'w') as tsvfile:
+            tsvfile.write(data)
 
 
 if __name__ == '__main__':
