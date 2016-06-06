17d048ba6b9403d8358509a09d3f0af8a90d5c92
Arne de Laat <adelaat@nikhef.nl>
2016-06-06 17:44:06 +0200

    Fix orientation as string in Detector

--- sapphire/clusters.py
+++ sapphire/clusters.py
@@ -44,9 +44,9 @@ class Detector(object):
             self.x = [position[0]]
             self.y = [position[1]]
             self.z = [position[2]] if len(position) == 3 else [0.]
-        if orientation is 'UD':
+        if orientation == 'UD':
             self.orientation = [0] * len(self.x)
-        elif orientation is 'LR':
+        elif orientation == 'LR':
             self.orientation = [pi / 2] * len(self.x)
         else:
             if hasattr(orientation, "__len__"):
