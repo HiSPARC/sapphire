148f1751bae1aa2329a554005276c6496a907751
Arne de Laat <adelaat@nikhef.nl>
2016-06-16 16:26:40 +0200

    Bump version to 1.0.0
    Add module simulating detection of gammas in the detector.
    Add direction reconstruction with curved shower front.
    Add timing offsets as local data for faster reconstructions.
    Add using offsets in coincidence direction reconstruction.
    Add instructions about SAPPHiRE development to README.
    Move time_deltas groups inside the coincidences group.
    More support for force stale/fresh.
    Prevent stray warnings during tests.
    Remove (unused) methods from API.
    Fix many tiny bugs, typos, and possible exceptions.
    Many thanks to @davidfokkema and @tomkooij !

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] != (2, 7):
 
 
 setup(name='hisparc-sapphire',
-      version='0.15.2',
+      version='1.0.0',
       packages=find_packages(),
       url='http://github.com/hisparc/sapphire/',
       bugtrack_url='http://github.com/HiSPARC/sapphire/issues',
