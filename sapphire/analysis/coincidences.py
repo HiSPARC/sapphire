5a6b7b53f91901ec6bdb28640156632e4cba762a
Tom Kooij <tomkooij@tomkooij.nl>
2016-06-29 15:43:15 +0200

    Revert "Add __future__ import. Use absolute imports"
    This reverts commit e16c88d916d5c614978132358f0ecb9125f10226.
    Relative imports are fine.

--- sapphire/analysis/coincidences.py
+++ sapphire/analysis/coincidences.py
@@ -30,17 +30,15 @@
             coin.search_and_store_coincidences()
 
 """
-from __future__ import print_function
-
 import os.path
 
 import tables
 import numpy as np
 from progressbar import ProgressBar, ETA, Bar, Percentage
 
-from sapphire.analysis import process_events
-from sapphire import storage
-from sapphire.utils import pbar
+from . import process_events
+from .. import storage
+from ..utils import pbar
 
 
 class Coincidences(object):
