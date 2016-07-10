ae5155883591b289ff9861116519b8b3c1bf753e
Arne de Laat <arne@delaat.net>
2016-07-10 10:43:13 +0200

    Remove unneeded characters and improve comments in units

--- sapphire/corsika/units.py
+++ sapphire/corsika/units.py
@@ -64,34 +64,30 @@ kSqrt2 = 1.4142135623730950488017
 kSqrt3 = 1.7320508075688772935274
 kSqrt2Pi = 2.5066282746310005024158
 
-#
 # Prefixes
-#
-yocto = 1.e-24
-zepto = 1.e-21
-atto = 1.e-18
-femto = 1.e-15
-pico = 1.e-12
-nano = 1.e-9
-micro = 1.e-6
-milli = 1.e-3
-centi = 1.e-2
-deci = 1.e-1
-deka = 1.e+1
-hecto = 1.e+2
-kilo = 1.e+3
-mega = 1.e+6
-giga = 1.e+9
-tera = 1.e+12
-peta = 1.e+15
-exa = 1.e+18
-zetta = 1.e+21
-yotta = 1.e+24
+yocto = 1e-24
+zepto = 1e-21
+atto = 1e-18
+femto = 1e-15
+pico = 1e-12
+nano = 1e-9
+micro = 1e-6
+milli = 1e-3
+centi = 1e-2
+deci = 1e-1
+deka = 1e1
+hecto = 1e2
+kilo = 1e3
+mega = 1e6
+giga = 1e9
+tera = 1e12
+peta = 1e15
+exa = 1e18
+zetta = 1e21
+yotta = 1e24
 
-#
 # Length [L]
-#
-meter = 1.0
+meter = 1.
 meter2 = meter * meter
 meter3 = meter * meter * meter
 
@@ -107,16 +103,16 @@ kilometer = kilo * meter
 kilometer2 = kilometer * kilometer
 kilometer3 = kilometer * kilometer * kilometer
 
-parsec = 3.0856775807e+16 * meter
+parsec = 3.0856775807e16 * meter
 kiloParsec = kilo * parsec
 megaParsec = mega * parsec
 
 micrometer = micro * meter
 nanometer = nano * meter
-angstrom = 1.e-10 * meter
+angstrom = 1e-10 * meter
 fermi = femto * meter
 
-barn = 1.e-28 * meter2
+barn = 1e-28 * meter2
 millibarn = milli * barn
 microbarn = micro * barn
 nanobarn = nano * barn
@@ -139,12 +135,10 @@ km = kilometer
 km2 = kilometer2
 km3 = kilometer3
 
-#
 # Angle
-#
 radian = 1.
 milliradian = milli * radian
-degree = (3.14159265358979323846 / 180.0) * radian
+degree = (3.14159265358979323846 / 180.) * radian
 
 steradian = 1.
 
@@ -154,9 +148,7 @@ mrad = milliradian
 sr = steradian
 deg = degree
 
-#
 # Time [T]
-#
 nanosecond = 1.
 nanosecond2 = nanosecond * nanosecond
 second = giga * nanosecond
@@ -176,16 +168,12 @@ ns = nanosecond
 s = second
 ms = millisecond
 
-#
 # Electric charge [Q]
-#
-eplus = 1.               # positron charge
+eplus = 1.  # positron charge
 eSI = 1.602176462e-19  # positron charge in coulomb
-coulomb = eplus / eSI        # coulomb = 6.24150 e+18 * eplus
+coulomb = eplus / eSI  # coulomb = 6.24150e18 * eplus
 
-#
 # Energy [E]
-#
 electronvolt = 1.
 megaelectronvolt = mega * electronvolt
 kiloelectronvolt = kilo * electronvolt
@@ -195,7 +183,7 @@ petaelectronvolt = peta * electronvolt
 exaelectronvolt = exa * electronvolt
 zettaelectronvolt = zetta * electronvolt
 
-joule = electronvolt / eSI  # joule = 6.24150 e+12 * MeV
+joule = electronvolt / eSI  # joule = 6.24150e12 * MeV
 
 # symbols
 MeV = megaelectronvolt
@@ -207,9 +195,7 @@ PeV = petaelectronvolt
 EeV = exaelectronvolt
 ZeV = zettaelectronvolt
 
-#
 # Mass [E][T^2][L^-2]
-#
 kilogram = joule * second * second / (meter * meter)
 gram = milli * kilogram
 milligram = milli * gram
@@ -219,114 +205,76 @@ kg = kilogram
 g = gram
 mg = milligram
 
-#
 # Power [E][T^-1]
-#
-watt = joule / second     # watt = 6.24150 e+3 * MeV/ns
+watt = joule / second  # watt = 6.24150e3 * MeV / ns
 
-#
 # Force [E][L^-1]
-#
-newton = joule / meter    # newton = 6.24150 e+9 * MeV/mm
+newton = joule / meter  # newton = 6.24150e9 * MeV / mm
 
-#
 # Pressure [E][L^-3]
-#
-#  hep_pascal = newton/m2         # pascal = 6.24150 e+3 * MeV/mm3
-#  pascal = hep_pascal        # pascal = 6.24150 e+3 * MeV/mm3
-pascal = newton / m2         # pascal = 6.24150 e+3 * MeV/mm3
-bar = 100000 * pascal     # bar = 6.24150 e+8 * MeV/mm3
-atmosphere = 101325 * pascal     # atm = 6.32420 e+8 * MeV/mm3
+pascal = newton / m2  # pascal = 6.24150e3 * MeV / mm3
+bar = 100000 * pascal  # bar = 6.24150e8 * MeV / mm3
+atmosphere = 101325 * pascal  # atm = 6.32420e8 * MeV / mm3
 
 # symbols
 hPa = hecto * pascal
 
-#
 # Electric current [Q][T^-1]
-#
-ampere = coulomb / second  # ampere = 6.24150 e+9 * eplus/ns
+ampere = coulomb / second  # ampere = 6.24150e9 * eplus / ns
 milliampere = milli * ampere
 microampere = micro * ampere
 nanoampere = nano * ampere
 
-#
 # Electric potential [E][Q^-1]
-#
 megavolt = megaelectronvolt / eplus
 kilovolt = milli * megavolt
 volt = micro * megavolt
 
-#
 # Electric resistance [E][T][Q^-2]
-#
-ohm = volt / ampere       # ohm = 1.60217e-16*(MeV/eplus)/(eplus/ns)
+ohm = volt / ampere  # ohm = 1.60217e-16 * (MeV / eplus) / (eplus / ns)
 
-#
 # Electric capacitance [Q^2][E^-1]
-#
-farad = coulomb / volt  # farad = 6.24150e+24 * eplus/Megavolt
+farad = coulomb / volt  # farad = 6.24150e24 * eplus / megavolt
 millifarad = milli * farad
 microfarad = micro * farad
 nanofarad = nano * farad
 picofarad = pico * farad
 
-#
 # Magnetic Flux [T][E][Q^-1]
-#
-weber = volt * second     # weber = 1000*megavolt*ns
+weber = volt * second  # weber = 1000 * megavolt * ns
 
-#
 # Magnetic Field [T][E][Q^-1][L^-2]
-#
-tesla = volt * second / meter2  # tesla =0.001*megavolt*ns/mm2
+tesla = volt * second / meter2  # tesla = 0.001 * megavolt * ns / mm2
 
-gauss = 1.e-4 * tesla
+gauss = 1e-4 * tesla
 kilogauss = deci * tesla
 
-#
 # Inductance [T^2][E][Q^-2]
-#
-henry = weber / ampere    # henry = 1.60217e-7*MeV*(ns/eplus)**2
+henry = weber / ampere  # henry = 1.60217e-7 * MeV * (ns / eplus) ** 2
 
-#
 # Temperature
-#
 kelvin = 1.
 
-#
 # Amount of substance
-#
 mole = 1.
 
-#
 # Activity [T^-1]
-#
 becquerel = 1. / second
-curie = 3.7e+10 * becquerel
+curie = 3.7e10 * becquerel
 
-#
 # Absorbed dose [L^2][T^-2]
-#
 gray = joule / kilogram
 
-#
 # Luminous intensity [I]
-#
 candela = 1.
 
-#
 # Luminous flux [I]
-#
 lumen = candela * steradian
 
-#
 # Illuminance [I][L^-2]
-#
 lux = lumen / meter2
 
-#
 # Miscellaneous
-#
 perCent = 0.01
 percent = perCent
 perThousand = 0.001
