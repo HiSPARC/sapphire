b60a08bc3f95599a54526851fc6cf5738f7e7ec5
Arne de Laat <arne@delaat.net>
2016-07-18 14:29:20 +0200

    Add Lightning data download to esd module
    Add lightning data TSV to be loaded via load_data
    Support downloading via download_data and provide
    download_lightning for convenience.
    Make context managers subclasses, DRY.
    Add tests for lightning data.

--- sapphire/tests/esd_load_data.py
+++ sapphire/tests/esd_load_data.py
@@ -14,6 +14,8 @@ test_data_coincidences_path = os.path.join(self_path,
                                            'test_data/esd_coincidence_data.h5')
 events_source = os.path.join(self_path, 'test_data/events-s501-20120101.tsv')
 weather_source = os.path.join(self_path, 'test_data/weather-s501-20120101.tsv')
+lightning_source = os.path.join(self_path,
+                                'test_data/lightning-knmi-20150717.tsv')
 coincidences_source = os.path.join(self_path,
                                    'test_data/coincidences-20160310.tsv')
 
@@ -33,6 +35,7 @@ def perform_load_data(filename):
     with tables.open_file(filename, 'w', filters=filters) as datafile:
         esd.load_data(datafile, '/', events_source, 'events')
         esd.load_data(datafile, '/', weather_source, 'weather')
+        esd.load_data(datafile, '/', lightning_source, 'lightning')
 
 
 def perform_esd_download_data(filename):
@@ -40,13 +43,17 @@ def perform_esd_download_data(filename):
 
     filters = tables.Filters(complevel=1)
     start = datetime.datetime(2012, 1, 1, 0, 0, 0)
-    eind = datetime.datetime(2012, 1, 1, 0, 1, 0)
+    end = datetime.datetime(2012, 1, 1, 0, 1, 0)
+    lightning_start = datetime.datetime(2015, 7, 17, 0, 0, 0)
+    lightning_end = datetime.datetime(2015, 7, 17, 0, 10, 0)
 
     with tables.open_file(filename, 'w', filters=filters) as datafile:
-        esd.download_data(datafile, '/', 501, start, eind, type='events',
+        esd.download_data(datafile, '/', 501, start, end, type='events',
                           progress=False)
-        esd.download_data(datafile, '/', 501, start, eind, type='weather',
+        esd.download_data(datafile, '/', 501, start, end, type='weather',
                           progress=False)
+        esd.download_lightning(datafile, '/', 4, lightning_start,
+                               lightning_end, progress=False)
 
 
 def perform_load_coincidences(filename):
@@ -62,11 +69,11 @@ def perform_download_coincidences(filename):
 
     filters = tables.Filters(complevel=1)
     start = datetime.datetime(2016, 3, 10, 0, 0, 0)
-    eind = datetime.datetime(2016, 3, 10, 0, 1, 0)
+    end = datetime.datetime(2016, 3, 10, 0, 1, 0)
 
     with tables.open_file(filename, 'w', filters=filters) as datafile:
         esd.download_coincidences(datafile, stations=[501, 510],
-                                  start=start, end=eind, n=2, progress=False)
+                                  start=start, end=end, n=2, progress=False)
 
 
 def create_and_store_test_data():
@@ -80,6 +87,9 @@ def create_and_store_test_data():
     urlretrieve('http://data.hisparc.nl/data/501/events/'
                 '?download=True&start=2012-01-01&end=2012-01-01+00:01:00',
                 events_source)
+    urlretrieve('http://data.hisparc.nl/data/knmi/lightning/4/'
+                '?download=True&start=2015-07-17&end=2015-07-17+00:10:00',
+                lightning_source)
     urlretrieve('http://data.hisparc.nl/data/network/coincidences/'
                 '?download=True&start=2016-03-10&end=2016-03-10+00:01:00'
                 '&stations=501,+510&n=2',
