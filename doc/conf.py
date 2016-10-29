a9e300a7c8174bfd4ae6bf85808455b47c5d1651
Arne de Laat <adelaat@nikhef.nl>
2016-10-29 17:16:50 +0200

    Make Sphinx docs conf clearer
    Remove many unused options.
    Change a few options; no link to source, no 'Created using..' text.
    Add Tom to authors.

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,9 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+# Often reused
+AUTHORS = u'David Fokkema, Arne de Laat, and Tom Kooij'
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -41,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SAPPHiRE'
-copyright = u'2009-2015, David Fokkema, Arne de Laat'
+copyright = u'2009-2016, {authors}'.format(authors=AUTHORS)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -76,10 +79,6 @@ exclude_patterns = ['_build']
 # unit titles (such as .. function::).
 #add_module_names = True
 
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
@@ -93,20 +92,12 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'nature'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -122,46 +113,11 @@ html_favicon = '_static/favicon.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# If false, no module index is generated.
-#html_domain_indices = True
-
-# If false, no index is generated.
-#html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
-
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
+html_show_sphinx = False
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'SAPPHiREdoc'
@@ -186,28 +142,12 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'SAPPHiRE.tex', u'SAPPHiRE Documentation',
-   u'David Fokkema, Arne de Laat', 'manual'),
+   AUTHORS, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 latex_logo = 'logo/logo.pdf'
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
-
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
-
-# If false, no module index is generated.
-#latex_domain_indices = True
 
 
 # -- Options for manual page output --------------------------------------------
@@ -216,11 +156,8 @@ latex_logo = 'logo/logo.pdf'
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'sapphire', u'SAPPHiRE Documentation',
-     [u'David Fokkema, Arne de Laat'], 1)
+     [AUTHORS], 1)
 ]
-
-# If true, show URL addresses after external links.
-#man_show_urls = False
 
 
 # -- Options for Texinfo output ------------------------------------------------
@@ -230,62 +167,19 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'SAPPHiRE', u'SAPPHiRE Documentation',
-   u'David Fokkema, Arne de Laat', 'SAPPHiRE',
+   AUTHORS, 'SAPPHiRE',
    'One line description of project.',
    'Miscellaneous'),
 ]
-
-# Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
-
-# If false, no module index is generated.
-#texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
 
 
 # -- Options for Epub output ---------------------------------------------------
 
 # Bibliographic Dublin Core info.
 epub_title = u'SAPPHiRE'
-epub_author = u'David Fokkema, Arne de Laat'
-epub_publisher = u'David Fokkema, Arne de Laat'
-epub_copyright = u'2009-2015, David Fokkema, Arne de Laat'
-
-# The language of the text. It defaults to the language option
-# or en if the language is not set.
-#epub_language = ''
-
-# The scheme of the identifier. Typical schemes are ISBN or URL.
-#epub_scheme = ''
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#epub_identifier = ''
-
-# A unique identification for the text.
-#epub_uid = ''
-
-# A tuple containing the cover image and cover page html template filenames.
-#epub_cover = ()
-
-# HTML files that should be inserted before the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_pre_files = []
-
-# HTML files shat should be inserted after the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_post_files = []
-
-# A list of files that should not be packed into the epub file.
-#epub_exclude_files = []
-
-# The depth of the table of contents in toc.ncx.
-#epub_tocdepth = 3
-
-# Allow duplicate toc entries.
-#epub_tocdup = True
+epub_author = AUTHORS
+epub_publisher = AUTHORS
+epub_copyright = u'2009-2016, {authors}'.format(authors=AUTHORS)
 
 
 # -- User-defined options ------------------------------------------------------
