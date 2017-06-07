d92094ceed6168aea9efed2d115f8e47cbed53b9
Edekje <ethanvanwoerkom@yahoo.com>
2017-06-07 12:45:21 +0200

    Set up celestial override by celestial_astropy

--- sapphire/transformations/__init__.py
+++ sapphire/transformations/__init__.py
@@ -20,14 +20,34 @@ Easy transformations between different systems.
 :mod:`~sapphire.transformations.geographic`
     geographic coordinate transformations (e.g. WGS84 to ENU)
 
+:mod:`~sapphire.transformations.celestial_astropy`
+
+
 """
 from . import angles
 from . import axes
 from . import base
-from . import celestial
 from . import clock
 from . import geographic
 
+"""
+    Celestial is an old, deprecated package, it has now been
+    replaced with more accurate astropy transformations.
+    
+    If astropy is present, then celestial_astropy will be used
+    and then celestial will be overridden.
+    
+    If it is not present
+    then celestial will be used so that users without astropy
+    may still use SAPPHIRE and a warning will be issued.
+"""
+try:
+    import astropy
+    from . import celestial_astropy as celestial
+except ImportError:
+    from . import celestial
+    import warnings
+    warnings.warn("Astropy not present, falling back on deprecated celestial", ImportWarning)
 
 __all__ = ['angles',
            'axes',
