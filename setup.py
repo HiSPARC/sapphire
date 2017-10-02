250952becbf6aa4d9eaea0e87d364e977a529f7c
Tom Kooij <tomkooij@tomkooij.nl>
2017-10-02 13:08:41 +0200

    Bump version to v1.5.1
    Update local data
    Fix test_calibration for new (changed) local data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 # set version number and write to sapphire/version.py
-version = '1.5.0'
+version = '1.5.1'
 
 version_py = """\
 # Created by setup.py. Do not edit.
